8d25f2e3c731aa9cc4bfe0a9ec8fec02c0db6fba
Sergius92739 <sergem279@gmail.com>
2021-10-23 16:23:24 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import Widget from './Widget';
 
-const widget = new Widget('http://localhost:7070/sse');
+const widget = new Widget('https://salty-bayou-98244.herokuapp.com/');
 
 widget.bindToDOM(document.querySelector('.page__list'));
 widget.init();
